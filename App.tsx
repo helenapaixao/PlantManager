ccad127eb15110d4784e5a42d2d6ebd7cca685c5
Helena Paixão <hp.helenapaixao@gmail.com>
2021-04-24 12:10:48 -0300

    :fire:feat: page welcome: done

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
+import {Text,View} from 'react-native'
 import React from 'react';
 import { Welcome } from './src/pages/Welcome';
+import AppLoading from 'expo-app-loading'
+
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost'
 
 
 export default function App() {
-  return (
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
 
+  if(!fontsLoaded) 
+  return (
+    <AppLoading/>
+  )
+  return (
+    
     <Welcome/>
    
   );
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,41 +1,52 @@
-import React,{useState} from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native'
-import colors from '../../styles/colors';
-import wateringImg from '../assets/watering.png'
+import React, { useState } from 'react';
+import 
+{ View, 
+  Text, 
+  Image, 
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions
+} from 'react-native'
 
-import Button from '../components/Button'
+import colors from '../../styles/colors';
+import wateringImg from '../assets/watering.png';
+import { Feather} from '@expo/vector-icons';
+import fonts from '../../styles/fonts';
+
 
 
 export function Welcome() {
-  const [visible, setVisible] = useState(false);
-
-
-  function handleVisibility() {
-setVisible(true)
-  }
   return (
-
-    <View style={styles.container}>
+          <View style={styles.container}>
       <Text style={styles.title}>Gerencie {'\n'}
-      suas plantas{'\n'}
-       de
-      forma fácil</Text>
+      suas plantas    de{ '\n'}
+    
+      forma fácil</Text>    
+        <Image
+         source={wateringImg} 
+         style={styles.image}
+         resizeMode="contain"
+          />
+      
 
-      {
-        visible && 
-     <Image source={wateringImg} style={styles.image} />
-      }
 
- 
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
-</Text>
-      <Button title=">" onPress={handleVisibility}/>
+        </Text>
+        <TouchableOpacity style={styles.button}
+        activeOpacity={0.7}
 
+      >
+    
+          <Feather name="chevron-right" style={styles.buttonIcon}    />
+      
+      </TouchableOpacity>
     </View>
 
+
+  
   )
 }
 
@@ -43,25 +54,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around'
   },
+
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
     marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
   },
-
   image: {
-    width: 293,
-    height: 284
+
+    height: Dimensions.get('window').width * 0.7
   },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 32
+  }
 
 })
