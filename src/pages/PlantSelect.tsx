bfd91a23f83cc095ef3c75a683c2315aeb262597
Helena Paixão <hp.helenapaixao@gmail.com>
2021-05-10 15:20:20 -0300

    feat: add config services with api

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,53 +1,76 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { View, Text, StyleSheet, FlatList
+import {
+  View, Text, StyleSheet, FlatList
 
 
 } from 'react-native';
 import colors from '../../styles/colors'
 import fonts from '../../styles/fonts';
 import { EnviromentButton } from '../components/EnviromentButton';
-import {Header} from '../components/Header'
+import { Header } from '../components/Header'
+import api from '../services/api';
 
 
+
+interface EnviromentProps {
+  key: string,
+  title: string
+}
 
 export function PlantSelect() {
-const navigation = useNavigation();
-  function handleMoveOn() {
+  const [enviroments, setEnviroments] = useState<EnviromentProps[]>([
 
-    navigation.navigate('PlantSelect')
-  }
+  ]);
+
+  useEffect(() => {
+    async function fetchEnviroment() {
+      const { data } = await api.get('plants_environments')
+      setEnviroments([
+        {
+          key: 'all',
+          title: 'Todos',
+        },
+
+        ...data
+      ])
+    }
+
+    fetchEnviroment();
+
+  }, [])
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-      <Header/>
-      <Text style={styles.title}>
-        Em qual ambiente
+        <Header />
+        <Text style={styles.title}>
+          Em qual ambiente
       </Text>
-      <Text style={styles.subTitle}>
-        você quer colocar sua planta?
+        <Text style={styles.subTitle}>
+          você quer colocar sua planta?
       </Text>
 
-      <View>
-        <FlatList
-          data={[1,2,3,4,5]}
-          renderItem={({item}) => (
-            <EnviromentButton 
-            title="Cozinha" 
-            active
-            />
-          )}
-          horizontal
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={styles.enviromentList}
+        <View>
+          <FlatList
+            data={enviroments}
+            renderItem={({ item }) => (
+              <EnviromentButton
+                title={item.title}
 
-        />
+              />
+            )}
+            horizontal
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={styles.enviromentList}
+
+          />
+        </View>
+
+
       </View>
 
-  
-      </View>
-     
     </View>
   )
 }
@@ -55,7 +78,7 @@ const navigation = useNavigation();
 const styles = StyleSheet.create({
   container: {
     flex: 1,
- 
+
     backgroundColor: colors.background
   },
 
@@ -83,7 +106,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingBottom: 5,
     marginLeft: 32,
-    marginVertical: 22,
+    marginVertical: 2,
 
   }
 })
